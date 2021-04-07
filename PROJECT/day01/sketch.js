b7f0db12f7a9892f874a4d0dc9581d52c1375353
eduairet <hola@eduairet.com>
2021-04-07 01:00:41 -0500

    A
    A done

--- PROJECT/day01/sketch.js
+++ PROJECT/day01/sketch.js
@@ -1,7 +1,19 @@
+let aRender;
+
+function preload() {
+  aRender = loadImage('aRender.png');
+}
+
 function setup() {
   createCanvas(1080, 1080);
 }
 
 function draw() {
   background(0);
+  tint(random(255), map(mouseX, 0, 255, 0, width), map(mouseY, 0, 255, 0, height)); 
+  image(aRender, 0, 0);
+}
+
+function mousePressed() {
+  saveFrames('day1', 'png', 1, 24);
 }
