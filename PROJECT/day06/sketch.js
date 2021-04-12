bdc9e96aaff4130af2186407e04550601bcdebe6
eduairet <hola@eduairet.com>
2021-04-11 20:25:13 -0500

    G done

--- PROJECT/day06/sketch.js
+++ PROJECT/day06/sketch.js
@@ -1,7 +1,79 @@
+const shapeSize = 162;
+const d = 10;
+let lp = false;
+
+Number.prototype.pad = function(size) {
+  let s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
+
 function setup() {
   createCanvas(1080, 1080);
+  noStroke();
+  frameRate(24);
 }
 
 function draw() {
+  const sDist = shapeSize/10;
+  let contourx = width*.7;
+  let barx = width*.6;
+  let contoury = height*.25;
+  let contourd = frameCount/12;
+
   background(0);
+
+  fv = 0
+  while (contourx > width*.4) {
+    contourx -= sDist;
+    if (barx > width*.45) {
+      barx -= sDist;
+    }
+    shape(contourx, contoury, fv, contourd);
+    shape(barx, height*.6, fv, contourd);
+    fv += 1
+  }
+  while (contoury < height*1.1) {
+    contoury += sDist;
+    shape(contourx, contoury, fv, contourd);
+    fv += 1
+  }
+
+  if (frameCount <= 76 ) {
+    save(`day6_${frameCount.pad(2)}.png`);
+  }
+
+  if (!lp) {
+    noLoop();
+  }
+}
+
+function shape(xt, yt, fv, deg, density=d, size=shapeSize) {
+  let fillVar = fv;
+  push(); 
+    translate(xt, yt);
+    rotate(deg)
+    for (let x=0; x < density; x++) {
+      for (let y=0; y < density; y++) {
+        f = 255;
+        if (fillVar%2 === 0) {
+          f = 0;
+        }
+        fill(f);
+        rect(x*(size/density), y*(size/density), size/density);
+        fillVar += 1;
+      }
+      fillVar += 1;
+    }
+  pop();
+
+}
+
+function mousePressed() {
+  lp = true;
+  loop();
+}
+
+function mouseReleased() {
+  lp = false;
 }
--- PROJECT/day07/sketch.js
+++ PROJECT/day07/sketch.js
@@ -1,7 +1,43 @@
+
+let h, w, stColor;
+let m = .1;
+let r = 20;
+
 function setup() {
   createCanvas(1080, 1080);
+  noFill();
+  setAttributes('antialias', true);
+  strokeCap(ROUND);
+  strokeJoin(ROUND);
+  w = width;
+  h = height;
+  background(0);
+  frameRate(12);
+  stColor = ['#000000', '#FFFFFF'];
+  blendMode(DIFFERENCE);
 }
 
 function draw() {
-  background(0);
+  translate(random(-r, r), random(-r, r));
+  stroke(random(stColor));
+  strokeWeight(random(width*.5));
+  beginShape();
+    //Bar
+    vertex(w*m*5.5, h*m*5.5);
+    bezierVertex(w*m*8, h*m*5.5, w*m*8, h*m*5.5, w*m*9, h*m*5.5);
+    //Curve
+    bezierVertex(w*m*9, h*m*8.3, w*m*6.5, h*m*9, w*m*5.1, h*m*9);
+    bezierVertex(w*m*2.5, h*m*9, w*m, h*m*7, w*m, h*m*4.9);
+    bezierVertex(w*m, h*m*2.7, w*m*2.5, h*m, w*m*5.1, h*m);
+    bezierVertex(w*m*6.2, h*m*1, w*m*7.7, h*m*1.4, w*m*8.8, h*m*2.9);
+  endShape();
+
+  if (frameCount <= 24*5 ) {
+    save(`day7_${pad(frameCount, 3)}.png`);
+  }
+}
+
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
 }
