18d919c04e14d90887cd8782861dd4674aa60e1b
Pamela Landgrave <landgravesp@gmail.com>
2021-04-16 21:00:24 -0500

    N Render

--- PROJECT/day14/sketch.js
+++ PROJECT/day14/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(1080, 1080);
+let ene;
+
+function preload() {
+    enee = createVideo(['./NRender_A.mp4']);
+}
+
+function setup(){
+    createCanvas(1080, 1080);
+
+    ene.play();
+    ene.loop();
+    ene.volume(0);
+    ene.hide();
+    
+    
 }
 
 function draw() {
-  background(0);
+    background(0);
+    image(elle, 0, 0);
 }
