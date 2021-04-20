564686d0fc30e0c35e41bc2d688789611a7bb088
eduairet <hola@eduairet.com>
2021-04-20 10:01:51 -0500

    P done

--- PROJECT/day16/sketch.js
+++ PROJECT/day16/sketch.js
@@ -1,7 +1,30 @@
+let banana;
+let z = 0;
+let speed = 4;
+
+function preload() {
+  banana = loadModel('./platano.obj');
+}
+
 function setup() {
-  createCanvas(1080, 1080);
+  createCanvas(1080, 1080, WEBGL);
+  frameRate(12);
+  noStroke();
+  
 }
 
 function draw() {
-  background(0);
+  background(50, 0, 0);
+  orbitControl();
+  push();
+    scale(25);
+    rotateX(-16);
+    translate(0, -15 + sin(frameCount)*-10, z);
+    emissiveMaterial(255, 255, 0)
+    model(banana);
+  pop();
+  z += speed;
+  if (z > 40 || z < 0) {
+    speed *= -1;
+  }
 }
