1746217c2a2de28254aa1bc89b59c676653c250a
Pamela Landgrave <landgravesp@gmail.com>
2021-04-23 11:53:20 -0500

    R done

--- PROJECT/day18/sketch.js
+++ PROJECT/day18/sketch.js
@@ -1,7 +1,18 @@
-function setup() {
-  createCanvas(1080, 1080);
+let mar; 
+
+function preload() {
+    mar = createVideo(['./0001_1.mp4']);
+}
+
+function setup(){
+    createCanvas(1080, 1080);
+    mar.play();
+    mar.loop();
+    mar.volume(0);
+    mar.hide();
 }
 
 function draw() {
-  background(0);
+    background(0);
+    image(mar, 0, 0);
 }
