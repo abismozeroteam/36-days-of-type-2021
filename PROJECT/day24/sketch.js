c345bf6a6a0e9806ff271c164657000db8022f07
eduairet <hola@eduairet.com>
2021-04-27 15:57:56 -0500

    X done

--- PROJECT/day24/sketch.js
+++ PROJECT/day24/sketch.js
@@ -1,7 +1,42 @@
+const stWght = 2;
+const frames = 12;
+let inc = 0;
+let speed = .1;
+
 function setup() {
   createCanvas(1080, 1080);
+  frameRate(frames);
+  angleMode(RADIANS);
+  noFill();
+  strokeWeight(stWght);
 }
 
 function draw() {
   background(0);
+  translate(width/2, height/2);
+  push();
+    for (let i=-width; i<width; i+=2) {
+      // push();
+      //   stroke(255, 0, 255);
+      //   translate(i*stWght, i*stWght);
+      //   line(width*.5, -height*.5, -width*.5, height*.5);  
+      // pop();
+      // push();
+      //   stroke(0, 255, 255);
+      //   translate(i*stWght, -i*stWght);
+      //   line(-width*.5, -height*.5, width*.5, height*.5);
+      // pop();
+      push();
+        stroke(255);
+        rotate(frameCount*.00006);
+        translate(sin(i*stWght) * width*inc, cos(i*stWght) * width*inc);
+        line(-width*.5, -height*.5, width*.5, height*.5);
+        line(width*.5, -height*.5, -width*.5, height*.5);
+      pop();
+    }
+  pop();
+  inc += speed;
+  if(inc >= 1 || inc <= 0) {
+    speed *= -1;
+  }
 }
