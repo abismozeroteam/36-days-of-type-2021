55fbcf4820732ac31b24e6aa39867600c62814ab
Pamela Landgrave <landgravesp@gmail.com>
2021-05-06 08:52:45 -0500

    5 done

--- PROJECT/day32/sketch.js
+++ PROJECT/day32/sketch.js
@@ -1,7 +1,15 @@
-function setup() {
+let render; 
+
+function preload() {
+  render = loadImage('./5Render.png');
+}
+
+function setup(){
   createCanvas(1080, 1080);
 }
 
 function draw() {
   background(0);
+  image(render, 0, 0);
 }
+
